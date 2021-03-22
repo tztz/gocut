e02ad49f91005f5e708e99ce8d9b08da5378613c
Mark Vz <markimo-the-dev@tztz.io>
2021-03-22 18:55:43 +0100

    Add comment

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+# Install linter
 # binary will be $(go env GOPATH)/bin/golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.38.0
