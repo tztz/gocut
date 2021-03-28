429ef815b90261839d07b173cb561e684bc5313e
Mark Vz <markimo-the-dev@tztz.io>
2021-03-28 22:11:23 +0200

    Make linter happy

--- internal/pkg/config/logconfig_test.go
+++ internal/pkg/config/logconfig_test.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,21 +11,21 @@ import (
 func TestInitLogConfig(t *testing.T) {
 	os.Setenv("RUN_PROFILE", "prod")
 	InitLogConfig()
-	_, ok := log.StandardLogger().Formatter.(*logrus.JSONFormatter)
+	_, ok := log.StandardLogger().Formatter.(*log.JSONFormatter)
 	assert.True(t, ok)
 
 	os.Setenv("RUN_PROFILE", "")
 	InitLogConfig()
-	_, ok = log.StandardLogger().Formatter.(*logrus.JSONFormatter)
+	_, ok = log.StandardLogger().Formatter.(*log.JSONFormatter)
 	assert.True(t, ok)
 
 	os.Setenv("RUN_PROFILE", "dev")
 	InitLogConfig()
-	_, ok = log.StandardLogger().Formatter.(*logrus.TextFormatter)
+	_, ok = log.StandardLogger().Formatter.(*log.TextFormatter)
 	assert.True(t, ok)
 
 	os.Setenv("RUN_PROFILE", "test")
 	InitLogConfig()
-	_, ok = log.StandardLogger().Formatter.(*logrus.TextFormatter)
+	_, ok = log.StandardLogger().Formatter.(*log.TextFormatter)
 	assert.True(t, ok)
 }
