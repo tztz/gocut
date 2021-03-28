99ea2a742d20b9e52a83e358cc05e5699ea83b1d
Mark Vz <markimo-the-dev@tztz.io>
2021-03-28 20:34:11 +0200

    Improve tests, error handling

--- internal/pkg/config/appconfig_test.go
+++ internal/pkg/config/appconfig_test.go
@@ -1,30 +1,68 @@
 package config
 
 import (
+	"errors"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAppConfigFileShouldBeReadAndHaveProp(t *testing.T) {
-	// always start fresh:
+func assertProp(t *testing.T, configsPath string, value int) {
+	t.Helper()
+
 	viper.Reset()
+	err := InitAppConfigForPath(configsPath)
+	assert.Nil(t, err, "should have initialized app config successfully but did not work")
+	assert.Equal(t, value, viper.Get("foo.bar"), "could not find app property 'foo.bar'")
+}
 
-	InitAppConfig()
-	err := ReadAppConfig()
+func assertNotFound(t *testing.T, path string) {
+	t.Helper()
 
-	assert.Nil(t, err, "should have read app config successfully but did not work")
+	viper.Reset()
+	err := InitAppConfigForPath(path)
+	var e viper.ConfigFileNotFoundError
+	assert.True(t, errors.As(err, &e), "should not have found app config but it exists")
+	assert.True(t, strings.HasPrefix(err.Error(), "app config file not found for profile"))
+}
 
-	assert.Equal(t, 815, viper.Get("foo.bar"), "could not find app property 'foo.bar'")
+func TestAppConfigFileShouldBeReadAndHaveProp(t *testing.T) {
+	configsPath := getProjectRoot() + "/test/configs"
+
+	os.Setenv("RUN_PROFILE", "test")
+	assertProp(t, configsPath, 815)
+
+	os.Setenv("RUN_PROFILE", "dev")
+	assertProp(t, configsPath, 5551234)
+
+	os.Setenv("RUN_PROFILE", "prod")
+	assertProp(t, configsPath, 4711)
+
+	os.Setenv("RUN_PROFILE", "")
+	assertProp(t, configsPath, 4711)
 }
 
 func TestViperShouldReturnErrorIfAppConfigFileNotFound(t *testing.T) {
-	// always start fresh:
+	os.Setenv("RUN_PROFILE", "test")
+	assertNotFound(t, "/tmp/foo-does-not-exist")
+
+	os.Setenv("RUN_PROFILE", "foo-does-not-exist")
+	assertNotFound(t, "/tmp/foo-does-not-exist")
+
+	os.Setenv("RUN_PROFILE", "foo-does-not-exist")
+	assertNotFound(t, "")
+}
+
+func TestViperShouldReturnErrorIfAppConfigFileFoundButCorrupt(t *testing.T) {
+	configsPath := getProjectRoot() + "/test/configs"
+
 	viper.Reset()
-
-	err := ReadAppConfig()
-
-	_, isExpectedError := err.(viper.ConfigFileNotFoundError)
-	assert.True(t, isExpectedError, "should not have found app config but it exists")
+	os.Setenv("RUN_PROFILE", "corrupt")
+	err := InitAppConfigForPath(configsPath)
+	var e viper.ConfigParseError
+	assert.True(t, errors.As(err, &e), "should have found corrupt app config but it is valid")
+	assert.True(t, strings.HasPrefix(err.Error(), "app config file found but corrupt for profile [corrupt]: "))
 }
