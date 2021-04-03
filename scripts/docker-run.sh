70a053c7fec109638c123fb30e20ad1e42ea779c
Mark Vz <markimo-the-dev@tztz.io>
2021-04-03 11:01:42 +0200

    Little improvements

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GIN_MODE=debug # default: release
+#GIN_MODE=debug # default: release
 
 # Run production Docker container locally with "prod" profile.
-docker run -e GIN_MODE=${GIN_MODE} --rm --publish 3000:3000 gocut
+docker run -e GIN_MODE=${GIN_MODE} --rm --publish 8080:8080 gocut
